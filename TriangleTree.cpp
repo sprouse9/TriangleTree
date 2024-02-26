45bbecc76996ce2bc68a55dd38713732258cebd3
sprouse9 <sprouse9@gmail.com>
2024-02-26 11:29:38 -0500

    Realized the problem is more profound than I initially thought
    Will update this code to traverse ALL the nodes to find the Max value.

--- TriangleTree.cpp
+++ TriangleTree.cpp
@@ -15,7 +15,20 @@ Write a program in a language of your choice to find the maximum total from top 
 containing a triangle with 100 rows. 
 Send your solution and resume to [123456 AT yodle dot com], replacing 123456 with the maximum sum for the triangle. 
 ********************************************************************************************************************************************************************************/
+/* UPDATE 02/26/204
+   After looking at this problem more carefully, I realized I need to traverse all possible paths in order to get the
+   maximum total. I made some assumptions about the initial problem that didn't tell the whole story.
+   If in the tree example above we change 8 -> 81, the current algorithm will not visit that node and not give us the 
+   max value we are looking for:
 
+        5
+      9   6
+    4   6   81
+  0   7   1   5
+
+I.e. 5 + 9 + 81 + 5 = 100
+
+*/
 
 // TriangleTree.cpp : Defines the entry point for the console application.
 //
